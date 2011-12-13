927008d0759eccf71b890fe7269c9cbf7ac1f887
Rim Zaidullin <tinybit@yandex.ru>
2011-12-14 01:32:37 +0300

    added missing files

---
+++ include/lsd/forwards.hpp
@@ -0,0 +1,11 @@
+#ifndef _LSD_CLIENT_FORWARD_HPP_INCLUDED_
+#define _LSD_CLIENT_FORWARD_HPP_INCLUDED_
+
+namespace lsd {
+
+class client_impl;
+template<typename LSD_T> class service;
+
+} // namespace lsd
+
+#endif // _LSD_CLIENT_FORWARD_HPP_INCLUDED_

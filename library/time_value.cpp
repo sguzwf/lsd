3858200ed21fdfde963ec6d3a9cd51bdfa934af9
Rim Zaidullin <tinybit@yandex.ru>
2011-12-14 13:30:44 +0300

    updated build, added proper unit tests configuration

--- library/time_value.cpp
+++ library/time_value.cpp
@@ -1,8 +1,6 @@
-#include "settings.h"
-
 #include <cstddef>
 
-#include "time_value.hpp"
+#include "details/time_value.hpp"
 
 namespace lsd {
 
@@ -14,14 +12,14 @@ time_value::time_value(const time_value& tv) {
 	*this = tv;
 }
 
-virtual ~time_value() {
+time_value::~time_value() {
 
 }
 
 time_value
 time_value::get_current_time() {
-	timeval tv;
-	gettimeofday(&tv, NULL);
+	time_value tv;
+	//gettimeofday(&tv, NULL);
 
 	return tv;
 }

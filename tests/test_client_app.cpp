a992ff1bbd45f0a5fd7a24c1860e32308f184c6d
Rim Zaidullin <tinybit@yandex.ru>
2011-12-16 11:21:51 +0300

    separated test client and unit tests

---
+++ tests/test_client_app.cpp
@@ -0,0 +1,90 @@
+//
+// Copyright (C) 2011 Rim Zaidullin <creator@bash.org.ru>
+//
+// Licensed under the BSD 2-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <iostream>
+#include <boost/program_options.hpp>
+
+#include <msgpack.hpp>
+
+#include "lsd/client.hpp"
+
+namespace po = boost::program_options;
+
+std::string config_path = "config_example.json";
+
+void create_client(int add_messages_count) {
+	lsd::client c(config_path);
+	c.connect();
+
+	// create message path
+	lsd::message_path path;
+	path.service_name = "karma-engine-testing";
+	path.handle_name = "event";
+
+	// create message policy
+	lsd::message_policy policy;
+	policy.deadline = 2.0;
+
+	// create message data
+	std::map<std::string, int> event;
+	event["service"] = 1;
+	event["uid"] = 12345;
+	event["score"] = 500;
+
+	msgpack::sbuffer buffer;
+	msgpack::pack(buffer, event);
+
+	// send messages
+	for (int i = 0; i < add_messages_count; ++i) {
+		std::string uuid1 = c.send_message(buffer.data(), buffer.size(), path, policy);
+	}
+
+	sleep(300);
+}
+
+int
+main(int argc, char** argv) {
+	try {
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help", "Produce help message")
+			("client,c", "Start as server")
+			("messages,m", po::value<int>(), "Add messages to server")
+		;
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			std::cout << desc << std::endl;
+			return EXIT_SUCCESS;
+		}
+
+		if (vm.count("client")) {
+			int add_messages_count = vm.count("messages") ? vm["messages"].as<int>() : 0;
+			create_client(add_messages_count);
+
+			return EXIT_SUCCESS;
+		}
+
+		std::cout << desc << std::endl;
+		return EXIT_FAILURE;
+	}
+	catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -19,52 +19,11 @@
 #include <boost/test/test_case_template.hpp>
 #include <boost/thread.hpp>
 
-#include <msgpack.hpp>
-
-#include "lsd/client.hpp"
 #include "details/time_value.hpp"
 
 typedef boost::mpl::list<int, long, unsigned char> test_types;
 
 BOOST_AUTO_TEST_SUITE(test_cached_read);
-
-void spawn_client() {
-	std::string config_path = "config_example.json";
-	int add_messages_count = 300;
-
-	lsd::client client(config_path);
-	client.connect();
-
-	// create message path
-	lsd::message_path path;
-	path.service_name = "karma-engine-testing";
-	path.handle_name = "event";
-
-	// create message policy
-	lsd::message_policy policy;
-	policy.deadline = 2.0;
-
-	// create message data
-	std::map<std::string, int> event;
-	event["service"] = 1;
-	event["uid"] = 12345;
-	event["score"] = 500;
-
-	msgpack::sbuffer buffer;
-	msgpack::pack(buffer, event);
-
-	// send messages
-	for (int i = 0; i < add_messages_count; ++i) {
-		std::string uuid1 = client.send_message(buffer.data(), buffer.size(), path, policy);
-	}
-
-	sleep(30);
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(first_test, T, test_types) {
-	//spawn_client();
-	BOOST_CHECK_EQUAL(1 == 1, true);
-}
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(time_value_test1, T, test_types) {
 	lsd::time_value tv;
